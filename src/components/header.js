b1f4634d9891572a7bcc26dc6ad068810f6a9dc2
SumitSPDC <sumit.singh@powerweave.com>
2024-05-17 10:11:45 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+const Header = () => (
+  <header>
+    <ul>
+      <li>
+        <Link legacyBehavior href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link legacyBehavior href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+      <li>
+        <Link legacyBehavior href="/post/[id]" as="/post/first">
+          <a>First Post</a>
+        </Link>
+      </li>
+      <li>
+        <Link legacyBehavior href="/post/[id]" as="/post/second">
+          <a>Second Post</a>
+        </Link>
+      </li>
+    </ul>
+  </header>
+)
+
+export default Header
